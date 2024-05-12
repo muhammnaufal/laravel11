510f2f28deb254629d41180513e9d1ff7be9619c
muhnaufal219@gmail.com <muhnaufal219@gmail.com>
2024-05-12 19:18:36 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('/pengguna',\App\Http\Controllers\UserController::class);
+Route::resource('/kelas',\App\Http\Controllers\KelasController::class);
+Route::resource('/matakuliah',\App\Http\Controllers\MatakuliahController::class);
